885f195f7dc1b69627f728487c03a71dbd50ae86
thana19 <thana.j@gmail.com>
2021-07-31 03:12:23 +0700

    readme

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -10,6 +13,72 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormBuilderState>();
+  late SharedPreferences sharedPref;
+
+  _initSharedPreferences() async {
+    sharedPref = await SharedPreferences.getInstance();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initSharedPreferences();
+  }
+
+  _login(var values) async {
+    var url = Uri.parse('https://api.thana.in.th/workshop/login');
+    var response = await http.post(url,
+        headers: {'Content-Type': 'application/json'},
+        body: convert.jsonEncode(
+            {'username': values['username'], 'password': values['password']}));
+    var body = convert.jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      final snackBar =
+          SnackBar(content: Text('${values['username']} is Logged In'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+      //save token to pref
+      await sharedPref.setString('token', response.body);
+      print(sharedPref.getString('token'));
+
+      _getProfile(body);
+    } else {
+      final snackBar = SnackBar(content: Text(body['message']));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+
+  _getProfile(var token) async {
+    //http get profile
+    var url = Uri.parse('https://api.thana.in.th/workshop/getprofile');
+    var response = await http.get(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ${token['access_token']}',
+      },
+    );
+    var body = convert.jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      print('statusCode == 200');
+      //save profile to pref
+      await sharedPref.setString('profile', response.body);
+      print(sharedPref.getString('profile'));
+
+      //open Launcher
+      Navigator.pushNamed(
+        context,
+        '/launcher',
+      );
+    } else {
+      print('get profile fail');
+      print(body['message']);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,8 +136,7 @@ class _LoginPageState extends State<LoginPage> {
                             onPressed: () {
                               _formKey.currentState!.save();
                               if (_formKey.currentState!.validate()) {
-                                // open Homepage (pass launcher)
-                                Navigator.pushNamed(context, '/launcher');
+                                _login(_formKey.currentState!.value);
                               } else {
                                 print("validation failed");
                               }
