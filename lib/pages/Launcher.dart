99f68f5002a90f63562fbca3977dc54f687f29e4
thana19 <thana.j@gmail.com>
2021-07-31 13:17:57 +0700

    bottomnavigation

--- lib/pages/Launcher.dart
+++ lib/pages/Launcher.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_workshoptest/pages/AboutPage.dart';
+import 'package:flutter_workshoptest/pages/HomePage.dart';
+import 'package:flutter_workshoptest/pages/ProfilePage.dart';
 
 class Launcher extends StatefulWidget {
   Launcher({Key? key}) : super(key: key);
@@ -8,17 +12,34 @@ class Launcher extends StatefulWidget {
 }
 
 class _LauncherState extends State<Launcher> {
+  int _selectedIndex = 0;
+  List<Widget> _pageWidget = [HomePage(), ProfilePage(), AboutPage()];
+
+  List<BottomNavigationBarItem> _menuBar = <BottomNavigationBarItem>[
+    BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.home), label: 'Home'),
+    BottomNavigationBarItem(
+        icon: Icon(FontAwesomeIcons.userAlt), label: 'Profile'),
+    BottomNavigationBarItem(
+        icon: Icon(FontAwesomeIcons.infoCircle), label: 'About'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Launcher'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text('Launcher')],
-          ),
-        ));
+      body: _pageWidget.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _menuBar,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
